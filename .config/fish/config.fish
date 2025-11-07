9bd0fbc908020ed542a56182c5fd674a050fb350
Thusan Arul <piruthusan@gmail.com>
2025-11-07 13:19:39 +0100

    hello

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,19 +14,25 @@ abbr --set-cursor='%' -- config '$EDITOR ~/.config/%'
 # lazygit
 abbr lg lazygit
 
+# rustrover
+abbr rustrover open -na "RustRover.app" --args nosplash
+
 # Activates vi mode bindings for fish and rebinds ctrl+c to clear input line
 # https://fishshell.com/docs/current/cmds/bind.html#examples
 # https://fishshell.com/docs/current/cmds/fish_vi_key_bindings.html
 # NOTE: any unmatched bindings sends an escape sequence (which will switch from insert to default/command mode): https://github.com/fish-shell/fish-shell/issues/3739#issuecomment-272902998
 set -g fish_key_bindings fish_vi_key_bindings
+
+# todo: figure out if this is cmd+k
 bind -M insert \cc kill-whole-line repaint
 
 # option+backspace deletes backward word
 bind -M insert \e\x7F backward-kill-word
 
 
+mise activate fish | source
+zoxide init fish | source
+
 # direnv hook needs to be at the end of file 
 # https://direnv.net/docs/hook.html#fish
 direnv hook fish | source
-mise activate fish | source
-zoxide init fish | source
